b70d4816f724b1a79e60ef312e369fde4fa00d5b
markhorvath <markhorvath7@gmail.com>
2018-03-29 11:17:27 -0400

    lec33 1st componenet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>I'm a React App</h1>
+        <p>This is working</p>
+        <Person />
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'is this a React App!!!!'));
   }
 }
 
