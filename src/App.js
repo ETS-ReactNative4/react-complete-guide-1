18e9ed3faf64747d596fcef4af301089729b16fe
markhorvath <markhorvath7@gmail.com>
2018-03-30 11:25:46 -0400

    lec45 onchange events between state and props, input field, new namechange func in state

--- src/App.js
+++ src/App.js
@@ -20,7 +20,22 @@ class App extends Component {
     ]});
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: "Tom", age: 36},
+        {name: event.target.value, age: 30},
+        {name: "Joe", age: 28}
+      ]
+    });
+  }
+
   render() {
+    // <button onClick={() => this.switchNameHandler('Thomas')}>
+    // return is implied but hidden after the fat arrow func only when on one line,
+    // otherwise have to use curly braces
+    // also this allows to pass data directly into the function call, but this can
+    // be inefficient so dont use it if you dont have to, bind syntax is better
     return (
       <div className="App">
         <h1>I'm a React App</h1>
@@ -32,7 +47,9 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Tommy')} >MY Hobbies inclue fortnite </Person>
+          click={this.switchNameHandler.bind(this, 'Tommy')}
+          changed={this.nameChangeHandler} >MY Hobbies inclue fortnite
+        </Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}/>
