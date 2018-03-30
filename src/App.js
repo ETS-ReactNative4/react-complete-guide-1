f046b9ba059f702bc2932477db3e7dbee53444f2
markhorvath <markhorvath7@gmail.com>
2018-03-30 10:54:50 -0400

    lec44, state, props, setState, bind, state methods, prop using state methods, etc

--- src/App.js
+++ src/App.js
@@ -11,15 +11,31 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = (newName) => {
+    console.log("switchNameHandler clicked/called")
+    this.setState({persons: [
+      {name: newName, age: 36},
+      {name: "Jillian", age: 30},
+      {name: "Joseph", age: 28}
+    ]});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I'm a React App</h1>
         <p>This is working</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>MY Hobbies inclue fortnite </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Thomas')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Tommy')} >MY Hobbies inclue fortnite </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'is this a React App!!!!'));
