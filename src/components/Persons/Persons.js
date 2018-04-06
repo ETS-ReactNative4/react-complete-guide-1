4f0e4beefe23bf3ebd835372049ab2988fd9c53b
markhorvath <markhorvath7@gmail.com>
2018-04-06 14:54:22 -0400

    lec83 converted Person and Persons components from stateless to stateful

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person'
 
-const persons = (props) => props.persons.map((person, index) => {
-        return <Person
-      // Alternative to this would be click={this.deletePersonHandler.bind(this, index)}
-          click={() => props.clicked(index)}
-          name={person.name}
-          age={person.age}
-          key ={person.id}
-          changed={(event, id) => props.changed(event, person.id)}/>
-    });
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            return <Person
+              click={() => this.props.clicked(index)}
+              name={person.name}
+              age={person.age}
+              key ={person.id}
+              changed={(event, id) => this.props.changed(event, person.id)}/>
+        });
+    }
+}
 
-export default persons;
+export default Persons;
